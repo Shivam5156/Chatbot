8c0eb96454788771cf7244e681efcc24b6a77f74
shivam5156 <singhs36533@gmail.com>
2026-04-17 14:26:07 +0530

    fixed cookie bug

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -107,7 +107,7 @@ const login = async (req, res) => {
         //cookie parser
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production' ? 'none' : false,
+            secure: process.env.NODE_ENV === 'production' ? true : false,
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000
 
